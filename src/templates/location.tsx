4eda0badf859829d2ef80a049012f7f52a8cd545
apav-dev <apavlick@yext.com>
2023-12-15 12:24:05 -0500

    Remove unused environment variables

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -70,10 +70,6 @@ const Locations: Template<TemplateRenderProps<LocationsType>> = ({
       <h1>Entity Powered Page for Location entities</h1>
       <div>Entity Name: {name}</div>
       <h2>Testing Priority of Env Vars</h2>
-      <div>VAR1: {YEXT_PUBLIC_VAR1}</div>
-      <div>VAR2: {YEXT_PUBLIC_VAR2}</div>
-      <div>VAR3: {YEXT_PUBLIC_VAR3}</div>
-      <div>VAR4: {YEXT_PUBLIC_VAR4}</div>
     </>
   );
 };
