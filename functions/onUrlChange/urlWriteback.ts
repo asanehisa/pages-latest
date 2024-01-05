542649fef5930be7c86a75fa366ce5e980032e42
apav-dev <apavlick@yext.com>
2024-01-05 13:58:05 -0500

    Refactor urlWritebackPlugin function in urlWriteback.ts

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -3,11 +3,15 @@ import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-
 const pageUrlCustomField = "c_pagesUrl"; // Need to update to api name of CF storing prod URL for entities
 const API_KEY = "e28da53848a6e8ee815bd443a4eda720";
 
-const main = urlWritebackPlugin({
-  field: pageUrlCustomField,
-  apiKey: API_KEY,
-  environment: "prod",
-  v: "20221010",
-});
+const main = (args) => {
+  console.log(args);
+
+  urlWritebackPlugin({
+    field: pageUrlCustomField,
+    apiKey: API_KEY,
+    environment: "prod",
+    v: "20221010",
+  });
+};
 
 export default main;
