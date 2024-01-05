26191c6e47640c35396c655ae7b8bfa14750fe17
apav-dev <apavlick@yext.com>
2024-01-05 12:41:51 -0500

    Update API key in urlWriteback.ts

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -1,7 +1,7 @@
 import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.5/mod.ts";
 
 const pageUrlCustomField = "c_pagesUrl"; // Need to update to api name of CF storing prod URL for entities
-const API_KEY = MGMT_API_KEY;
+const API_KEY = "e28da53848a6e8ee815bd443a4eda720";
 
 const main = urlWritebackPlugin({
   field: pageUrlCustomField,
