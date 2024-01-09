0d9c85f9cfe423439652b9b0f8af0e6dec6b225e
apav-dev <apavlick@yext.com>
2024-01-09 10:37:45 -0500

    Refactor urlWriteback function and updateEntity function

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -3,22 +3,60 @@ import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-
 const pageUrlCustomField = "c_pagesUrl"; // Need to update to api name of CF storing prod URL for entities
 const API_KEY = "e28da53848a6e8ee815bd443a4eda720";
 
-const main = (args) => {
-  console.log(args);
+const main = urlWriteback({
+  field: pageUrlCustomField,
+  apiKey: API_KEY,
+  environment: "prod",
+  v: "20240108",
+});
 
-  try {
-    console.log("Running urlWritebackPlugin");
-    urlWritebackPlugin({
-      field: pageUrlCustomField,
-      apiKey: API_KEY,
-      environment: "prod",
-      v: "20221010",
+function urlWriteback(config) {
+  function onUrlChange(event) {
+    if (!(event.entityId && event.locale && event.url)) {
+      console.log("Invalid event", event);
+      return null;
+    }
+    let field = config.field;
+    if (config.featureToFieldMap && config.featureToFieldMap[event.feature]) {
+      field = config.featureToFieldMap[event.feature];
+    }
+    const update = {
+      [field]: `https://${event.url}`,
+    };
+    return updateEntity(event.entityId, event.locale, update, config.apiKey, {
+      v: config.v,
+      env: config.environment,
     });
-    console.log("Finished urlWritebackPlugin");
-  } catch (e) {
-    console.log("Error running urlWritebackPlugin");
-    console.log(e);
   }
-};
+  return onUrlChange;
+}
+
+function buildApiUrl(base, path, params) {
+  const result = new URL(path, base);
+  for (const k in params) {
+    result.searchParams.append(k, params[k]);
+  }
+  return result.toString();
+}
+async function updateEntity(id, locale, body, apiKey, options) {
+  const URL_BASE = "https://api.yext.com/v2/accounts/me/";
+  const url = buildApiUrl(URL_BASE, `entityprofiles/${id}/${locale}`, {
+    api_key: apiKey,
+    v: options?.v || "20220903",
+  });
+  const req = new Request(url, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+  });
+  const response = await fetch(req);
+  if (response.status < 200 || response.status >= 300) {
+    const responseBody2 = await response.json();
+    throw responseBody2.meta.errors[0].message;
+  }
+  const responseBody = await response.json();
+  console.log("Updated entity", responseBody.response);
+  return responseBody.response;
+}
 
 export default main;
