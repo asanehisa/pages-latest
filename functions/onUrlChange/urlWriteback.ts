5a1359bc88a6915fd70f2e06e0c66594055ab8f7
apav-dev <apavlick@yext.com>
2024-01-09 10:22:33 -0500

    Add error handling and logs to urlWritebackPlugin

--- functions/onUrlChange/urlWriteback.ts
+++ functions/onUrlChange/urlWriteback.ts
@@ -6,12 +6,19 @@ const API_KEY = "e28da53848a6e8ee815bd443a4eda720";
 const main = (args) => {
   console.log(args);
 
-  urlWritebackPlugin({
-    field: pageUrlCustomField,
-    apiKey: API_KEY,
-    environment: "prod",
-    v: "20221010",
-  });
+  try {
+    console.log("Running urlWritebackPlugin");
+    urlWritebackPlugin({
+      field: pageUrlCustomField,
+      apiKey: API_KEY,
+      environment: "prod",
+      v: "20221010",
+    });
+    console.log("Finished urlWritebackPlugin");
+  } catch (e) {
+    console.log("Error running urlWritebackPlugin");
+    console.log(e);
+  }
 };
 
 export default main;
