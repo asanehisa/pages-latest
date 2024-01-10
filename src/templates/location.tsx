f64e8d9bd105b48d22f2a3d99eb0481276636a4e
apav-dev <apavlick@yext.com>
2024-01-10 13:50:05 -0500

    Remove unused import and update assets directory in vite.config.js

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -10,7 +10,6 @@ import {
 } from "@yext/pages";
 import "../index.css";
 import { Locations as LocationsType } from "../types/autogen";
-import { Link } from "@yext/pages-components";
 
 export const config: TemplateConfig = {
   stream: {
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), yextSSG()],
-  // files at /public/prefix/assets
-  // publicDir: "/public",
+  // files at /public/locations/assets
   build: {
     assetsDir: "locations/assets",
-    // assetsInclude: ['**/*.ico'] // Include all .ico files as they are
   }
 });
