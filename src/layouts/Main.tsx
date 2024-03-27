d4a4699944eed8046ffeaeae1d0a4e18a99595d7
apav-dev <apavlick@yext.com>
2024-03-26 19:47:16 -0600

    Remove commented out code in Main component

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -8,13 +8,13 @@ export interface MainProps {
 
 export const Main = ({ children, templateData }: MainProps) => {
   return (
-    <AnalyticsProvider
-      apiKey="e28da53848a6e8ee815bd443a4eda720"
-      currency="USD"
-      templateData={templateData}
-      productionDomains={["pages-latest.pgsdemo"]}
-    >
-      <div className="relative min-h-screen">{children}</div>
-    </AnalyticsProvider>
+    // <AnalyticsProvider
+    //   apiKey="e28da53848a6e8ee815bd443a4eda720"
+    //   currency="USD"
+    //   templateData={templateData}
+    //   productionDomains={["pages-latest.pgsdemo"]}
+    // >
+    <div className="relative min-h-screen">{children}</div>
+    // </AnalyticsProvider>
   );
 };
